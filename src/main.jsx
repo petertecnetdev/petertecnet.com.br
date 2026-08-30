c267e81f1607289606b837899893ec63056f262f
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 21:58:08 -0300

    feat: start interaction telemetry for every session

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+import { startTelemetry } from './telemetry'
+import { getToken } from './admin/api'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+startTelemetry({ apiBaseUrl: 'https://api.petertecnet.com.br/api', appSlug: 'petertecnet', getToken })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
