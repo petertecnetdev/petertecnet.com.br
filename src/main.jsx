611a1964e8e3f3a12e5a2200e0fbb989ed7d6c9f
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:26:39 -0300

    feat(seo): activate landing index policy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './LandingEnhancements.css'
+import './seo.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
