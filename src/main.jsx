dbfeee9416246b64d99054db399865274def6d49
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:46:26 -0300

    feat: bootstrap Peter Account on ecosystem portal

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,21 @@ import './index.css'
 import './LandingEnhancements.css'
 import './seo.js'
 import App from './App.jsx'
+import PeterAccountGateway from './components/PeterAccountGateway.jsx'
+
+const API_BASE_URL = 'https://api.petertecnet.com.br/api'
+const APP_SLUG = 'peter-tecnet'
+
+const adminToken = localStorage.getItem('petertecnet_admin_token')
+const ecosystemToken = localStorage.getItem('token')
+
+if (adminToken && !ecosystemToken) localStorage.setItem('token', adminToken)
+if (ecosystemToken && !adminToken) localStorage.setItem('petertecnet_admin_token', ecosystemToken)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PeterAccountGateway apiBaseUrl={API_BASE_URL} appSlug={APP_SLUG}>
+      <App />
+    </PeterAccountGateway>
   </StrictMode>,
 )
