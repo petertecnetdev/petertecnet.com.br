a256a0d4adef7fd9a74f908187b3e532c558b875
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:48:17 -0300

    chore: keep gateway self-contained

--- src/components/peterAccountSecurity.js
+++
@@ -1,9 +0,0 @@
-export function isTrustedPeterTecnetUrl(value) {
-  try {
-    const url = new URL(value)
-    const host = url.hostname.toLowerCase()
-    return url.protocol === 'https:' && (host === 'petertecnet.com.br' || host.endsWith('.petertecnet.com.br'))
-  } catch {
-    return false
-  }
-}
