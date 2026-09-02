a4ba61d49e34f33db9e9db1b3f2501f4ab0da366
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 11:48:30 -0300

    refactor: remove legacy landing enhancement stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './LandingEnhancements.css'
 import './seo.js'
 import App from './App.jsx'
 import LaoraAdminCenter from './LaoraAdminCenter.jsx'
