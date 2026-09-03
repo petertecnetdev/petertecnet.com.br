0a985a92bed8cf73f69d074cb0cde695970efbe8
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 15:27:06 -0300

    refactor(admin): isolate navigation event contract

---
+++ src/admin/AdminUiEvents.js
@@ -0,0 +1,6 @@
+export const ADMIN_NAVIGATE_EVENT = 'peter-admin:navigate'
+export const ADMIN_NAVIGATION_CHANGED_EVENT = 'peter-admin:navigation-changed'
+
+export function requestAdminNavigation(key, options = {}) {
+  window.dispatchEvent(new CustomEvent(ADMIN_NAVIGATE_EVENT, { detail: { key, ...options } }))
+}
