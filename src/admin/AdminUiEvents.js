0a2687999a2a3f8f22035aebd42561e1ae6f461b
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 17:56:31 -0300

    Add Admin Center navigation lifecycle event

--- src/admin/AdminUiEvents.js
+++ src/admin/AdminUiEvents.js
@@ -1,3 +1,4 @@
+export const ADMIN_BEFORE_NAVIGATE_EVENT = 'peter-admin:before-navigate'
 export const ADMIN_NAVIGATE_EVENT = 'peter-admin:navigate'
 export const ADMIN_NAVIGATION_CHANGED_EVENT = 'peter-admin:navigation-changed'
 
