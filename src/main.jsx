4fb18185dfa5407ef0008005d3c56626b892a12a
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 10:04:44 -0300

    feat(admin): mount productivity bridge

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,10 @@ import AccountAccessPage from './AccountAccessPage.jsx'
 import PeterAccountGateway from './components/PeterAccountGateway.jsx'
 import AdminMobileNavigation from './components/AdminMobileNavigation.jsx'
 import AdminEstablishmentMediaBridge from './AdminEstablishmentMedia.jsx'
+import AdminProductivityBridge from './AdminProductivityBridge.jsx'
 import './AdminMobileNavigation.css'
 import './AdminDesktop.css'
+import './AdminProductivityBridge.css'
 
 const API_BASE_URL = 'https://api.petertecnet.com.br/api'
 const APP_SLUG = 'peter-tecnet'
@@ -35,6 +37,7 @@ const page = isLaoraAdmin ? (
     <App />
     {isAdmin && <AdminEstablishmentMediaBridge />}
     {isAdmin && <AdminMobileNavigation />}
+    {isAdmin && <AdminProductivityBridge />}
   </>
 )
 
