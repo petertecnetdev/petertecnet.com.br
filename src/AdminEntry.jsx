1c5b0c63f6ad7a5c895db58120d39eed4b8735bd
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 16:40:59 -0300

    perf: isola runtime público do Admin Center
    Separa o bundle público do runtime administrativo, reduzindo o custo inicial da landing sem regredir o Admin Center.

---
+++ src/AdminEntry.jsx
@@ -0,0 +1,44 @@
+import App from './App.jsx'
+import LaoraAdminCenter from './LaoraAdminCenter.jsx'
+import EcosystemLauncherAdmin from './EcosystemLauncherAdmin.jsx'
+import ApplicationBrandingPage from './ApplicationBrandingPage.jsx'
+import ContentDiscoveryAdminPage from './ContentDiscoveryAdminPage.jsx'
+import AdminMobileNavigation from './components/AdminMobileNavigation.jsx'
+import AdminEstablishmentMediaBridge from './AdminEstablishmentMedia.jsx'
+import AdminProductivityBridge from './AdminProductivityBridge.jsx'
+import AdminHomeBridge from './AdminHomeBridge.jsx'
+import AdminDeepLinkBridge from './AdminDeepLinkBridge.jsx'
+import AdminUiProvider from './admin/AdminUiProvider.jsx'
+import './admin/AdminTokens.css'
+import './AdminProductivityBridge.css'
+import './AdminProductivityLayoutFix.css'
+import './AdminVisualSystem.css'
+import './AdminExperience.css'
+import './admin/AdminNavigationState.css'
+
+function normalizePath(pathname = window.location.pathname) {
+  return String(pathname || '/').replace(/\/+$/, '') || '/'
+}
+
+export default function AdminEntry() {
+  const path = normalizePath()
+  const isLauncherAdmin = path === '/admin/ecosystem-launcher'
+  const isBrandingAdmin = path === '/admin/branding'
+  const isContentAdmin = path === '/admin/content'
+  const isLaoraAdmin = path.startsWith('/admin/laora')
+
+  let page
+  if (isLauncherAdmin) {
+    page = <EcosystemLauncherAdmin />
+  } else if (isBrandingAdmin) {
+    page = <><ApplicationBrandingPage /><AdminMobileNavigation /></>
+  } else if (isContentAdmin) {
+    page = <><ContentDiscoveryAdminPage /><AdminMobileNavigation /></>
+  } else if (isLaoraAdmin) {
+    page = <><LaoraAdminCenter /><AdminMobileNavigation /></>
+  } else {
+    page = <><App /><AdminEstablishmentMediaBridge /><AdminMobileNavigation /><AdminProductivityBridge /><AdminHomeBridge /><AdminDeepLinkBridge /></>
+  }
+
+  return <AdminUiProvider>{page}</AdminUiProvider>
+}
