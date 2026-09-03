e384beb7a693f1be997bf1c736ec09aeccd5426f
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 17:57:43 -0300

    Remove duplicate Admin Center navigation shell

--- src/Admin.js
+++ src/Admin.js
@@ -1,23 +1,15 @@
 import { createElement, Fragment } from 'react'
 import { AdminPage as LegacyAdminPage, LoginPage } from './Admin.jsx'
 import AdminCommercialWorkspace from './AdminCommercialWorkspaceV2.jsx'
-import AdminVisibilityPage, { AdminModuleNav } from './AdminVisibilityPage.jsx'
 import './AdminCommercialWorkspaceV2.css'
 import './AdminCommercialWorkspaceMobile.css'
 
 export { LoginPage }
 
 export function AdminPage() {
-  const path = window.location.pathname.replace(/\/+$/, '') || '/admin'
-
-  if (path === '/admin/visibility') {
-    return createElement(AdminVisibilityPage)
-  }
-
   return createElement(
     Fragment,
     null,
-    createElement(AdminModuleNav, { active: 'mission' }),
     createElement(LegacyAdminPage),
     createElement(AdminCommercialWorkspace)
   )
