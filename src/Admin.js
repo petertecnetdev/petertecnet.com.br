7e08f525e65d24bb0bf2c3332cfff4da9daf51ef
Peter Tecnet <petertecnet@gmail.com>
2026-09-04 10:59:32 -0300

    feat(admin): mount voice commands across admin center

--- src/Admin.js
+++ src/Admin.js
@@ -1,6 +1,7 @@
 import { createElement, Fragment } from 'react'
 import { AdminPage as LegacyAdminPage, LoginPage } from './Admin.jsx'
 import AdminCommercialWorkspace from './AdminCommercialWorkspaceV2.jsx'
+import VoiceAdminAssistant from './VoiceAdminAssistant.jsx'
 import './AdminCommercialWorkspaceV2.css'
 import './AdminCommercialWorkspaceMobile.css'
 
@@ -11,6 +12,7 @@ export function AdminPage() {
     Fragment,
     null,
     createElement(LegacyAdminPage),
-    createElement(AdminCommercialWorkspace)
+    createElement(AdminCommercialWorkspace),
+    createElement(VoiceAdminAssistant)
   )
 }
