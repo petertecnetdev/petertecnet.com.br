cbcf6bafff7b02b042220d5f66773ae4c35c515e
Peter Tecnet <petertecnet@gmail.com>
2026-09-04 19:03:41 -0300

    fix(admin): remove establishments modal behavior

--- src/Admin.js
+++ src/Admin.js
@@ -1,23 +1,16 @@
 import { createElement, Fragment } from 'react'
-import { createPortal } from 'react-dom'
 import { AdminPage as LegacyAdminPage, LoginPage } from './Admin.jsx'
-import AdminCommercialWorkspace from './AdminCommercialWorkspaceV2.jsx'
+import AdminEstablishmentsPageBridge from './AdminEstablishmentsPageBridge.jsx'
 import VoiceAdminAssistant from './VoiceAdminAssistant.jsx'
-import './AdminCommercialWorkspaceV2.css'
-import './AdminCommercialWorkspaceMobile.css'
 
 export { LoginPage }
 
 export function AdminPage() {
-  const commercialWorkspace = typeof document !== 'undefined'
-    ? createPortal(createElement(AdminCommercialWorkspace), document.body)
-    : createElement(AdminCommercialWorkspace)
-
   return createElement(
     Fragment,
     null,
     createElement(LegacyAdminPage),
-    commercialWorkspace,
+    createElement(AdminEstablishmentsPageBridge),
     createElement(VoiceAdminAssistant)
   )
 }
