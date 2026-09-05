34fb39f25c43b3ff85248a38e07cb5ff3ee935c1
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:44:50 -0300

    feat(admin): mount global search navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import AdminAppNavigation from './AdminAppNavigation.jsx'
+import AdminGlobalSearch from './AdminGlobalSearch.jsx'
 import AdminEstablishmentsIntegration from './AdminEstablishmentsIntegration.jsx'
 import AdminItemsIntegration from './AdminItemsIntegration.jsx'
 import './App.css'
@@ -12,6 +13,7 @@ import './AdminNavigationStandard.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <AdminGlobalSearch />
     <AdminEstablishmentsIntegration />
     <AdminItemsIntegration />
     <AdminAppNavigation />
