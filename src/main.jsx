c356191a7ab869bdb3197e554afd9d5d5d20795b
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:31:35 -0300

    style: load responsive admin hardening last

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import AdminEstablishmentsIntegration from './AdminEstablishmentsIntegration.jsx'
 import './App.css'
 import './AdminEstablishmentsFeedback.css'
+import './AdminResponsive.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
