dfeff28b449270cdaa9d9bf863592e7dfc69dc2d
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:40:38 -0300

    feat(admincenter): mount canonical app navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AdminAppNavigation from './AdminAppNavigation.jsx'
 import AdminEstablishmentsIntegration from './AdminEstablishmentsIntegration.jsx'
 import AdminItemsIntegration from './AdminItemsIntegration.jsx'
 import './App.css'
 import './AdminEstablishmentsFeedback.css'
 import './AdminResponsive.css'
+import './AdminNavigationStandard.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <AdminEstablishmentsIntegration />
     <AdminItemsIntegration />
+    <AdminAppNavigation />
   </StrictMode>,
 )
