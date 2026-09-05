d6117204c418ba01eca607995ed3824e470f63ae
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:23:17 -0300

    fix(admincenter): load establishment feedback styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import AdminEstablishmentsIntegration from './AdminEstablishmentsIntegration.jsx'
 import './App.css'
+import './AdminEstablishmentsFeedback.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
