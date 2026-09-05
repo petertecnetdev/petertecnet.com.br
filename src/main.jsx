558149abf04852b7bff3f4de138ab0346d09f43d
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:20:03 -0300

    feat(admincenter): mount establishments management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AdminEstablishmentsIntegration from './AdminEstablishmentsIntegration.jsx'
 import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <AdminEstablishmentsIntegration />
   </StrictMode>,
 )
