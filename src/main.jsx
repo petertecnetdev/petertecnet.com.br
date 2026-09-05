209e67b4ab2919d3f022adc60038e245c7f44244
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 14:45:34 -0300

    feat(admin): mount PWA installer globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import AdminSessionGuard from './AdminSessionGuard.jsx'
+import PwaInstallButton from './PwaInstallButton.jsx'
 import './App.css'
 import './AdminEstablishmentsFeedback.css'
 import './AdminResponsive.css'
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <AdminSessionGuard />
+    <PwaInstallButton />
   </StrictMode>,
 )
