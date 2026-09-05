d27eae48d994f924c323cabbe9494ec9341dfdb3
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 20:26:22 -0300

    feat(admin): tornar Visão Geral operacional e acionável
    Adiciona camada de inteligência operacional à Visão Geral com fila de decisões, pulso do ecossistema, atalhos, ranking de aplicações e atividade recente, integrada à navegação canônica e responsiva.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AdminOverviewIntelligence from './AdminOverviewIntelligence.jsx'
+import AdminOverviewNavigationBridge from './AdminOverviewNavigationBridge.jsx'
 import AdminSessionGuard from './AdminSessionGuard.jsx'
 import PwaInstallButton from './PwaInstallButton.jsx'
 import SupportAdminApp, { AdminSupportLauncher } from './SupportAdminApp.jsx'
@@ -15,7 +17,7 @@ const path = window.location.pathname.replace(/\/+$/, '') || '/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {path === '/support' ? <SupportAdminApp /> : <><App /><AdminSupportLauncher /></>}
+    {path === '/support' ? <SupportAdminApp /> : <><App /><AdminOverviewIntelligence /><AdminOverviewNavigationBridge /><AdminSupportLauncher /></>}
     <AdminSessionGuard />
     <PwaInstallButton />
   </StrictMode>,
