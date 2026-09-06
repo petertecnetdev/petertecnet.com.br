3022c656a4aa5dda995f1654cfb5a84e0fdb65a7
Peter Tecnet <petertecnet@gmail.com>
2026-09-06 17:02:00 -0300

    feat(conversion): enable resilient quote draft recovery

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,7 @@
 const path = window.location.pathname.replace(/\/+$/, '') || '/'
 
 import('./MarketingConversionBridge.js')
+import('./MarketingQuoteDraftBridge.js')
 import('./SupportLinkBridge.js')
 
 const isMarketingHub = path === '/'
