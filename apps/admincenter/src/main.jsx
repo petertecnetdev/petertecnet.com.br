a7ab942bc8501e68a1d28327e3a03aa4c262c438
Peter Tecnet <petertecnet@gmail.com>
2026-09-19 23:39:11 -0300

    fix(admincenter): remove floating support button

--- apps/admincenter/src/main.jsx
+++ apps/admincenter/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import PwaInstallButton from './PwaInstallButton.jsx'
-import SupportAdminApp, { AdminSupportLauncher } from './SupportAdminApp.jsx'
+import SupportAdminApp from './SupportAdminApp.jsx'
 import { installAdminRuntimeMonitor } from './adminRuntimeMonitor.js'
 import { installWebVitals } from './discoveryApi.js'
 import './admin.css'
@@ -14,7 +14,7 @@ const path = window.location.pathname.replace(/\/+$/, '') || '/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {path === '/support' ? <SupportAdminApp /> : <><App /><AdminSupportLauncher /></>}
+    {path === '/support' ? <SupportAdminApp /> : <App />}
     <PwaInstallButton />
   </StrictMode>,
 )
