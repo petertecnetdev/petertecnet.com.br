e95e5dda90b5fbfd56bdcbe35d35b8c7126d4c83
Peter Tecnet <petertecnet@gmail.com>
2026-09-21 22:35:36 -0300

    feat(brand): adopt new Peter Tecnet symbol

--- src/seoEntities.js
+++ src/seoEntities.js
@@ -2,7 +2,7 @@ export const organizationSeo = {
   name: 'Peter Tecnet',
   legalName: 'Peter Tecnet',
   url: 'https://petertecnet.com.br/',
-  logo: 'https://petertecnet.com.br/logopetertecnet.png',
+  logo: 'https://petertecnet.com.br/petertecnet-brand.svg',
   taxID: '42.595.409/0001-48',
   email: 'contato@petertecnet.com.br',
   instagram: 'https://www.instagram.com/petertecnet/',
